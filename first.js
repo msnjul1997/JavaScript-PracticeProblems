b2a7a975fa97acd8d7642ab287a2c96eb86f6c8b
msnkittu859 <Vamshikalyan97>
2022-07-18 13:48:22 +0530

    Added: Finding String Length and Adding Numbers

---
+++ first.js
@@ -0,0 +1,8 @@
+function add( a, b) {
+    return a+b;
+}
+function findLength(str){
+    return str.length;
+}
+console.log(add(10,12))
+console.log("The length of the string is: "+findLength("Santhosh"));
