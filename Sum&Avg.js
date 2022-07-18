edb75bbaa8723bfd3ef26184c8ff11935369cc59
msnkittu859 <Vamshikalyan97>
2022-07-18 13:59:42 +0530

    Added: Sum and Avg

---
+++ Sum&Avg.js
@@ -0,0 +1,10 @@
+const getRandomNumber = Math.floor(Math.random() * 6)+1
+var num1 = getRandomNumber
+var num2 = getRandomNumber
+var num3 = getRandomNumber
+var num4 = getRandomNumber
+var num5 = getRandomNumber
+var sum = num1+num2+num3+num4
+var avg = sum/5
+console.log("The sum is : "+sum)
+console.log("The avg is : "+avg)
